a2f72d0cd2c7f6544e220ba31ca805e684f45d1a
Ioan Eugen Stan <ieugen@apache.org>
2012-09-21 01:47:32 +0300

    DomainList is not immutable!!

---
+++ dom/src/test/java/org/apache/james/mime4j/dom/address/DomainListTest.java
@@ -0,0 +1,45 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mime4j.dom.address;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * DomainList is not immutable!!
+ */
+public class DomainListTest {
+
+    @Test
+    public void testFalseImmutability() throws Exception {
+        final List<String> list = new ArrayList<String>();
+        final DomainList domainList = new DomainList(list, true);
+
+        assertThat(domainList.size(), is(0));
+        list.add("fake");
+        list.add("immutability");
+        assertThat(domainList.size(), is(2));
+    }
+}
